7b6a7e792250e3ebe899ddd85306233edc5478e3
Joseph Tully <tullyj2@wwu.edu>
2019-04-15 17:53:46 -0700

    initial commit

---
+++ a1/app/src/main/java/tullyj2/csci412/wwu/tullyj2_a1/MainActivity.java
@@ -0,0 +1,13 @@
+package tullyj2.csci412.wwu.tullyj2_a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
